5bf8b1bae652b6d52e349be9abf8d9210d78f813
matt cameron <matt@mattcameron.me>
2025-04-14 17:27:15 +1000

    remove unused scripts

--- common/promptTemplates.js
+++
@@ -1,6 +0,0 @@
-import { PromptTemplate } from '@langchain/core/prompts';
-import { CONFIG } from './config.js';
-
-export function getSummaryTemplate() {
-  return PromptTemplate.fromTemplate(CONFIG.prompt.summary);
-}
--- common/vectorStore/generateEmbeddings.js
+++
@@ -1,7 +0,0 @@
-// common/vectorStore/generateEmbeddings.js
-import { OllamaEmbeddings } from '@langchain/ollama';
-
-export async function getEmbedding({ model }) {
-  // Instantiate the embeddings class using Ollama.
-  return new OllamaEmbeddings({ model });
-}
